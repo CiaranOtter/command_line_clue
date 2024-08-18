4dda13864c42dfae4d5ff7e332a8aea150e9b79f
Ciaran otter <ciaranjotter@gmail.com>
2024-08-18 21:43:15 +0200

    adding error messages for the game_system

--- server/game_service/game_service.go
+++ server/game_service/game_service.go
@@ -3,6 +3,7 @@ package gameservice
 import (
 	"context"
 	"database/sql"
+	"log"
 
 	"github.com/CiaranOtter/command_line_clue/server/clc_services/games"
 	"google.golang.org/grpc/codes"
@@ -17,9 +18,10 @@ type GameService struct {
 func (g GameService) RegisterNewGame(ctx context.Context, in *games.GameItem) (*games.Response, error) {
 	name := in.GetName()
 
-	_, err := g.DB.Exec("INSERT INTO registered_games (name) VALUES (%s)", name)
+	_, err := g.DB.Exec("INSERT INTO registered_games (name) VALUES ($1)", name)
 
 	if err != nil {
+		log.Printf(err.Error())
 		return nil, status.Error(codes.Canceled, "Failed to saved game into list of registered games")
 	}
 
